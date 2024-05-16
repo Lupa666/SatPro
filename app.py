98da04dd13a57a3a7ebb352852cbe85bc1999785
BSojka <bartkowski_so@interia.pl>
2024-05-16 22:37:18 +0200

    Expanded qam64 utils with functions to convert from string and int into encoded qam64 and tested them

--- app.py
+++ app.py
@@ -1,20 +1,34 @@
 import qam64_utils as qam64
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 from scipy.special import erfc
 
-coded_list = list()
-"010010010001111111"
-value = int("010010010001111111", 2)
-coded_list.append(qam64.ints_map.get(int(value%64)))
-value /= 64
-coded_list.append(qam64.ints_map.get(int(value%64)))
-value /= 64
-coded_list.append(qam64.ints_map.get(int(value%64)))
+
+
+test_stream = "000000000011101010010101110001111111"
+value = qam64.b2i(test_stream)
+size = math.ceil(len(test_stream)/6)
+
+string_output = 0
+int_output = 0
+int_output_size = 0
+
+string_output = qam64.encode_complex_string(test_stream)
+
+int_output = qam64.encode_complex_int(value)
+
+int_output_size = qam64.encode_complex_int(value, size)
+
+print("string", string_output)
+print("int no size",int_output)
+print("int size", int_output_size)
+
+
 
 # [ele.real for ele in data] 
-x = [ele.real for ele in coded_list]
-y = [ele.imag for ele in coded_list]
+x = [ele.real for ele in int_output_size]
+y = [ele.imag for ele in int_output_size]
 
 plt.scatter(x, y) 
 plt.xlabel('Real') 
@@ -24,13 +38,13 @@ plt.show()
 
 EbN0DB = np.arange(start=-10,stop=13,step=0.2)
 
-BER_eq = erfc(np.sqrt(10**(EbN0DB/10)))/2
+BER_eq = erfc(np.sqrt(10**(EbN0DB/10)))/qam64.M
 
 fig, ax = plt.subplots(nrows=1,ncols = 1)
 #ax.semilogy(EbN0dBs,BER_sim,color='r',marker='o',linestyle='',label='BPSK Sim')
-ax.semilogy(EbN0DB,BER_eq,marker='',linestyle='-',label='BPSK Theory')
+ax.semilogy(EbN0DB,BER_eq,marker='',linestyle='-',label='QAM64 Theory')
 ax.set_xlabel('$E_b/N_0(dB)$')
 ax.set_ylabel('BER ($P_b$)')
-ax.set_title('Probability of Bit Error for QAM64 over AWGN channel')
+ax.set_title('Probability of Bit Error for QAM64')
 ax.set_xlim(-5,13);ax.grid(True)
 ax.legend();plt.show()
--- qam64_utils.py
+++ qam64_utils.py
@@ -8,8 +8,15 @@ d = 2 # odległość amplitudy pomiędzy wartościami
 
 # "{0:b}".format(value) - int do stringa bitowego
 # int(value,2) - string bitowy do inta
-# /64 - przesuń bity o 6
+# //64 - przesuń bity o 6
 # %64 - wyciągnij 6 bitów
+
+def b2i(input:str):
+     return int(input,2)
+
+def i2b(input:int):
+     return "{0:b}".format(input)
+     
 
 map_string = dict()
 for imag in [-7j,-5j,-3j,-1j,1j,3j,5j,7j]:
@@ -58,3 +65,28 @@ ints_map = dict()
 
 for i in  map_string:
      ints_map[int( map_string[i],2)] = i
+
+# Returns a list of encoded complex values. Accepts a string.
+def encode_complex_string(input: str):
+    toReturn = list() 
+    while input != "":
+        toReturn.append(ints_map[int(input[-6:],2)])
+        input = input[:-6]
+        
+    return toReturn
+
+# Returns a list of encoded complex values. 
+# Accepts a number and size. 
+# If no size provided, last 0 might be omitted.
+# Size represents number of 6 bit strings we want to encode.
+def encode_complex_int(input: int, size = None):
+    toReturn = list() 
+    if size == None:
+        while input != 0:
+            toReturn.append(ints_map[input%64])
+            input //= 64
+        return toReturn
+    for i in range(size):
+         toReturn.append(ints_map[input%64])
+         input //= 64
+    return toReturn
